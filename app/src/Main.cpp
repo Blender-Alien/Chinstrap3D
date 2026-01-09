c70309a30286236dd7575ef4048f63962fd8cdf4
Leonhard Goethe <leonhard.goethe@gmail.com>
2026-01-09 13:50:54 +0100

    finish build setup

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Application.h"
 
 int main()
 {
-    std::cout << "Hello World!" << "\n";
+    Shader shader;
+    Shader shader2 = Shading::Bind(shader);
+    std::cout << shader2.m_Filepath << "\n";
+    std::cout << "Hello World!" << std::endl; 
 }
--- shell/generate.sh
+++ shell/generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cmake -G "Ninja Multi-Config" -B bin
+
+ln -s bin/compile_commands.json .
